b69b6cb13c075476ff697916552c52ae0d74bab9
eric-k-chu <kiyuen88ec@gmail.com>
2024-11-02 16:26:07 -0700

    moving to mono repo

--- vite-tailwind/src/lib/add.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { add } from './add'
-
-describe('add', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 2)).toBe(3)
-  })
-})
