411e63ef29949b40e88b3f8be5944db0d18d5ae7
eric-k-chu <kiyuen88ec@gmail.com>
2024-11-02 18:01:36 -0700

    veact

---
+++ templates/veact/src/App.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export function App(): ReactNode {
+  return (
+    <div className='flex min-h-screen items-center justify-center'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
